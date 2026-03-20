6ff12be5c5fcc3c314d8dd99367308c03a3a520b
allisonnakajima17 <allison.nakajima@paramount.com>
2026-03-20 17:01:54 -0400

    add prototype set up

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <p className="text-lg text-white/50">Ready to prototype</p>
+    </div>
+  )
+}
+
+export default App
